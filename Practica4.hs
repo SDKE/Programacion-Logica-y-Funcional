caa181bff040751c3a5ab8acd8751e11f24ff0d4
NeroRhoads <andreszip_1991@hotmail.com>
2016-02-26 23:43:26 -0600

    Update Practica4.hs
    foles se la sigue comiendo

--- Practica4.hs
+++ Practica4.hs
@@ -1,3 +1,9 @@
+-- torres de hanoi con instrucciones en string
+hanoi 0 a b c = putStr ""
+hanoi n a b c = do (hanoi (n-1) a c b)
+                   putStrLn ("mueve aro " ++ (show n) ++ " de " ++ (show a) ++" a " ++ (show c))
+                   (hanoi (n-1) b a c)
+
 borraElemento e [] = []
 borraElemento e (x:xs) = if (e==x) then xs else x:borraElemento e xs
 
