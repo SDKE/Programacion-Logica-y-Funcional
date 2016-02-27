f61f92c0687f0972921366b63a35b47291a2df71
Drakoon <cgonzalezserrano1494@gmail.com>
2016-02-26 23:31:45 -0600

    Update Practica4.hs

--- Practica4.hs
+++ Practica4.hs
@@ -1,6 +1,5 @@
 borraElemento e [] = []
-borraElemento e (x:xs) = if (e==x) then xs
-						else x:borraElemento e xs
+borraElemento e (x:xs) = if (e==x) then xs else x:borraElemento e xs
 
 permuta [] = [[]]
 permuta l = [x:xs | x <- l, xs <- permuta (borraElemento x l)]
