53381f2b214b32b8408e211385fa4d1aa966aebd
Alfredo Arenas <alfredo_avga@hotmail.com>
2016-02-27 14:04:00 -0600

    Update Practica4.hs

--- Practica4.hs
+++ Practica4.hs
@@ -6,10 +6,19 @@ comprime xs = aux 1 xs
           aux n [x] = [(x,n)]
 
 -- torres de hanoi con instrucciones en string
-hanoi 0 a b c = putStr ""
+{-hanoi 0 a b c = putStr ""
 hanoi n a b c = do (hanoi (n-1) a c b)
                    putStrLn ("mueve aro " ++ (show n) ++ " de " ++ (show a) ++" a " ++ (show c))
-                   (hanoi (n-1) b a c)
+                   (hanoi (n-1) b a c
+-}
+
+
+hanoi n a b c = zipWith (++) (lista n) (hanoi'' n a b c)
+lista n = [(show x)++".-" | x <- [1..n]]
+
+hanoi'' 0 _ _ _ = []
+hanoi'' n a b c = (hanoi'' (n-1) a c b)++["mueve aro " ++ (show n) ++ " de " ++ (a) ++" a " ++ (c)]++(hanoi'' (n-1) b a c)
+
 -- premutaciones de una lista
 borraElemento e [] = []
 borraElemento e (x:xs) = if (e==x) then xs else x:borraElemento e xs
