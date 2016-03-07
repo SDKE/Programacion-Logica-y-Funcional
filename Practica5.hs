128fb2147e4f17ed9c83cedbe78ed6dc652be046
Alfredo Arenas <alfredo_avga@hotmail.com>
2016-03-06 21:50:23 -0600

    Update Practica5.hs

--- Practica5.hs
+++ Practica5.hs
@@ -1,3 +1,16 @@
-grafo 0 _ = []
-grafo n l = revisa(head l) ++ (grafo (pred n) (tail l))
-            where revisa (c,a,b) = if(c=="une") then [True] else [False]
+
+grafo n l = grafo2 n [] l
+
+grafo2 0 _ _ = []
+grafo2 n g [] = []
+grafo2 n g ((c,a,b):xs) = if (c=="une") then [True] ++ (grafo2 (pred n) (g++[(a,b)]) xs) else (busca a b g)++(grafo2 n g xs)
+
+
+busca _ _ [] = []
+busca na nb ((a,b):xs)
+                    |((na==a)&&(nb==b)) = [True]
+                    |(na==a)= busca b nb (xs++[(a,b)])
+                    |otherwise = [False]
+
+
+
