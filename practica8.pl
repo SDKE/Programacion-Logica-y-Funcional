abbca89a5fe7ec5f7415501f624c4b6a47da2f35
NeroRhoads <andreszip_1991@hotmail.com>
2016-04-29 20:44:07 -0500

    by zack snyder
    falta generalista de 1 a n

---
+++ practica8.pl
@@ -0,0 +1,39 @@
+eliminaDuplicados([],[]).
+eliminaDuplicados([H|T],S):- member(H,T), !, eliminaDuplicados(T,S).
+eliminaDuplicados([H|T],[H|S]):- eliminaDuplicados(T,S).
+
+generaLista(0,[]).
+generaLista(N,[N|L]):- N1 is N-1, N2 is N1+1, generaLista(N2,L).
+
+combinaciones(_,0,[]).
+combinaciones([X|T],N,[X|Comb]):- N>0, N1 is N-1, combinaciones(T,N1,Comb).
+combinaciones([_|T],N,Comb):- N>0, combinaciones(T,N,Comb).  
+
+reinas(N,Sol):- generarTablero(N,Tablero),
+permutar(Tablero,Sol),
+buenTablero(Sol).
+
+generarTablero(0,[]).
+generarTablero(X,[X|R]):-
+XMenos1 is X - 1,
+XMenos1 >= 0,
+generarTablero(XMenos1,R).
+
+permutar([],[]).
+permutar(X,[C|Z]) :- seleccionar(X,C,R), permutar(R,Z).
+
+seleccionar([X|R],X,R).
+seleccionar([C|R],X,[C|Y]) :- seleccionar(R,X,Y).
+
+buenTablero([]).
+buenTablero([C|R]):- not(amenaza(C,R)),
+buenTablero(R).
+
+amenaza(X,Prof,[C|_]):- X is C+Prof;
+X is C-Prof;
+X = C.
+amenaza(X,Prof,[_|R]):- ProfMas1 is Prof + 1,
+amenaza(X,ProfMas1,R).
+
+amenaza(_,[]):- fail.
+amenaza(X,Y):- amenaza(X,1,Y).
