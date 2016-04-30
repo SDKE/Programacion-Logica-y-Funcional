e211d4a818938ca8978f8681b974f6e7a9cd3a24
NeroRhoads <andreszip_1991@hotmail.com>
2016-04-29 20:46:26 -0500

    actualizacion by zack snyder
    se agregaron comentarios

--- practica8.pl
+++ practica8.pl
@@ -2,13 +2,17 @@ eliminaDuplicados([],[]).
 eliminaDuplicados([H|T],S):- member(H,T), !, eliminaDuplicados(T,S).
 eliminaDuplicados([H|T],[H|S]):- eliminaDuplicados(T,S).
 
+%% falta que se genere la lista de 1 a N
 generaLista(0,[]).
 generaLista(N,[N|L]):- N1 is N-1, N2 is N1+1, generaLista(N2,L).
 
+%% falta que la salida sea una lista de listas
 combinaciones(_,0,[]).
 combinaciones([X|T],N,[X|Comb]):- N>0, N1 is N-1, combinaciones(T,N1,Comb).
 combinaciones([_|T],N,Comb):- N>0, combinaciones(T,N,Comb).  
 
+%% falta que la salida sea una lista de listas y en el autismo, que salga como en el ejemplo de la practica,
+%% sino sale ordenada de n a n-1
 reinas(N,Sol):- generarTablero(N,Tablero),
 permutar(Tablero,Sol),
 buenTablero(Sol).
